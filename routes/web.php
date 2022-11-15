aa858cb03f16a05de4e7c60301da79137b4467d7
Geekigen <kigencaleb50@gmail.com>
2022-11-15 11:59:44 +0300

    users and students cruid

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\studentcontroller;
+use App\Http\Controllers\Admin\usercontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+  Route::resource('user', usercontroller::class);
+Route::resource('student', studentcontroller::class);  
+});
