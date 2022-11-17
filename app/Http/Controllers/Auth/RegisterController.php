2e32a0aa9475eb132cdebe34484d35054e36960b
Geekigen <kigencaleb50@gmail.com>
2022-11-17 13:02:01 +0300

    fixed controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,7 +70,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
