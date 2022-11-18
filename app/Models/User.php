f24fb27da76062a69226f14cf65ed1324d06e412
Geekigen <kigencaleb50@gmail.com>
2022-11-18 12:48:56 +0300

    user model change

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable , SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -35,6 +37,10 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function student(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
     /**
      * The attributes that should be cast.
      *
